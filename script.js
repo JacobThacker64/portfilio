b976189c9f2d6277c1ed711226fbdb68e9be9285
JacobThacker64 <jthacker10@sycamores.indstate.edu>
2026-05-03 16:28:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    alert("Thanks for reaching out, Jacob Thacker will respond soon.");
+}
+
+function showProjectMessage() {
+    alert("This project is part of Jacob Thacker's portfolio.");
+}
